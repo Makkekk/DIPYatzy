d3cca761036f81449c1b9986d5c5bbb5409c6a53
Karsten <karsten-kirkegaard@hotmail.com>
2025-09-17 11:02:57 +0200

    YatzyGui

---
+++ Gui/domModification.js
@@ -0,0 +1,15 @@
+const dice = document.querySelectorAll(".die");
+const rollButton = document.getElementById("roll-button");
+ 
+
+dice.forEach(die => {
+  die.innerHTML = `<img src="images/dice6.svg" alt="Die 6">`;
+});
+
+
+function rollDice() {
+  dice.forEach(die => {
+    const value = Math.floor(Math.random() * 6) + 1; // random 1–6
+    die.innerHTML = `<img src="images/dice${value}.svg" alt="Die ${value}">`;
+  });
+}
